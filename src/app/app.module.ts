d82986d67172a486589111608bafcd44a3f880d1
kunvar singh <ankit@eiysys.in>
2020-03-04 06:11:11 -0500

    fix: added the code for local storage to fetch and set store value

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,10 +18,31 @@ import {
   createStore,
 } from 'redux';
 
+const localStorageMiddleware = ({getState}) => { // <--- FOCUS HERE
+    return (next) => (action) => {
+        const result = next(action);
+        localStorage.setItem('applicationState', JSON.stringify(
+            getState()
+        ));
+        return result;
+    };
+};
+
+
+const reHydrateStore = () => { // <-- FOCUS HERE
+
+    if (localStorage.getItem('applicationState') !== null) {
+        return JSON.parse(localStorage.getItem('applicationState')) // re-hydrate the store
+
+    }
+}
+
 
 export const store: Store<any> = createStore(
   rootReducer,
-  applyMiddleware(createLogger()),
+      reHydrateStore(),// <-- FOCUS HERE
+
+  applyMiddleware(localStorageMiddleware,createLogger()),
 );
 
 @NgModule({
